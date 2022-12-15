65b9ac9f253e8d9d3a9c13ba967bad2daa882ab9
Roman <generallevy45@mail.ru>
2022-12-15 17:20:50 +0300

    add hw-4

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,13 +1,55 @@
 import './Chats.css'
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
+import { Link } from 'react-router-dom'
+import { theme } from '../Form/Form'
+import IButton from '@mui/material/Button';
+import ITextField from '@mui/material/TextField';
+import { ThemeProvider } from '@mui/material/styles';
 
-export default function Chats({chats}) {
+export default function Chats({ addChat, chats }) {
+  const [value, setValue] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addChat({
+      id: nanoid(),
+      name: value
+    })
+  }
 
   return (
     <div className="chats">
       <h3>Список чатов:</h3>
-      <ul>{chats.map(el => (
-        <li key={el.id}>{el.name}</li>
-      ))}</ul>
+      <ul>
+        {chats.map(chat => (
+          <li key={chat.id}>
+            <Link to={`/chats/${chat.name}`}>
+              {chat.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+      <ThemeProvider theme={theme}>
+        <ITextField
+          id="outlined-basic"
+          label="write chat"
+          variant="outlined"
+          type="text"
+          onChange={(event) => setValue(event.target.value)}
+          color="primary"
+          size="small"
+        />
+        <IButton
+          variant="contained"
+          type="submit"
+          color="primary"
+        >
+          Add chat
+        </IButton>
+      </ThemeProvider>
+      </form>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import { Outlet, NavLink } from 'react-router-dom'
+import './Header.css'
+
+export const navigate = [
+  {
+    id: 1,
+    name: 'Main',
+    to: '/'
+  },
+  {
+    id: 2,
+    name: 'Profile',
+    to: '/profile'
+  },
+  {
+    id: 3,
+    name: 'Chat',
+    to: '/chats'
+  }
+]
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav className="header">
+          <ul>
+            {navigate.map(link => (
+              <li key={link.id}>
+                <NavLink
+                  to={link.to}
+                  style={({ isActive }) => ({
+                    color: isActive ? 'red' : 'blue'
+                  })}
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  )
+}
